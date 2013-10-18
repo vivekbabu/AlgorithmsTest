cb7f219c0fc503c152825582aeecf51528ef1f7a
Vivek <Vivek@Vivek-PC>
2013-10-18 12:53:40 +0530

    Modified Rational

--- AlgorithmsProject/.worksheet/src/in.algorithms.rationals.Rationals.scala
+++ AlgorithmsProject/.worksheet/src/in.algorithms.rationals.Rationals.scala
@@ -2,36 +2,61 @@ package in.algorithms.rationals
 
 object Rationals {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(89); 
   val rational1 = new Rational(2, 3);System.out.println("""rational1  : in.algorithms.rationals.Rational = """ + $show(rational1 ));$skip(37); 
-  val rational2 = new Rational(1, 2);System.out.println("""rational2  : in.algorithms.rationals.Rational = """ + $show(rational2 ));$skip(27); val res$0 = 
-  rational1.add(rational2);System.out.println("""res0: in.algorithms.rationals.Rational = """ + $show(res$0));$skip(33); val res$1 = 
-  rational1.add(rational2).neg();System.out.println("""res1: in.algorithms.rationals.Rational = """ + $show(res$1));$skip(27); val res$2 = 
-  rational1.sub(rational2);System.out.println("""res2: in.algorithms.rationals.Rational = """ + $show(res$2));$skip(31); 
+  val rational2 = new Rational(1, 2);System.out.println("""rational2  : in.algorithms.rationals.Rational = """ + $show(rational2 ));$skip(24); val res$0 = 
+  rational1 + rational2;System.out.println("""res0: in.algorithms.rationals.Rational = """ + $show(res$0));$skip(25); val res$1 = 
+  rational1 + -rational2;System.out.println("""res1: in.algorithms.rationals.Rational = """ + $show(res$1));$skip(24); val res$2 = 
+  rational1 - rational2;System.out.println("""res2: in.algorithms.rationals.Rational = """ + $show(res$2));$skip(31); 
 
   val x = new Rational(1, 3);System.out.println("""x  : in.algorithms.rationals.Rational = """ + $show(x ));$skip(29); 
   val y = new Rational(5, 7);System.out.println("""y  : in.algorithms.rationals.Rational = """ + $show(y ));$skip(29); 
-  val z = new Rational(3, 2);System.out.println("""z  : in.algorithms.rationals.Rational = """ + $show(z ));$skip(18); val res$3 = 
-  x.add(y).mul(z);System.out.println("""res3: in.algorithms.rationals.Rational = """ + $show(res$3))}
+  val z = new Rational(3, 2);System.out.println("""z  : in.algorithms.rationals.Rational = """ + $show(z ));$skip(14); val res$3 = 
+
+  x + y * z;System.out.println("""res3: in.algorithms.rationals.Rational = """ + $show(res$3));$skip(8); val res$4 = 
+  x < y;System.out.println("""res4: Boolean = """ + $show(res$4));$skip(11); val res$5 = 
+  x.max(y);System.out.println("""res5: in.algorithms.rationals.Rational = """ + $show(res$5));$skip(37); 
+
+  val strange = new Rational(1, 1);System.out.println("""strange  : in.algorithms.rationals.Rational = """ + $show(strange ));$skip(18); val res$6 = 
+  new Rational(5);System.out.println("""res6: in.algorithms.rationals.Rational = """ + $show(res$6))}
 }
 
 class Rational(x: Int, y: Int) {
-  def numer = x
-  def denom = y
+  def this(x: Int) = this(x, 1)
+  require(y != 0, "Denominator must not be zero")
 
-  def add(that: Rational): Rational = {
+  val numer = x
+  val denom = y
+
+  def +(that: Rational): Rational = {
     new Rational(this.numer * that.denom + this.denom * that.numer, this.denom * that.denom)
   }
 
-  def neg(): Rational = {
+  def unary_- : Rational = {
     new Rational(-this.numer, this.denom)
   }
 
-  def sub(that: Rational): Rational = {
-    add(that.neg)
+  def -(that: Rational): Rational = {
+    this + -that
   }
 
-  def mul(that: Rational): Rational = {
+  def *(that: Rational): Rational = {
     new Rational(this.numer * that.numer, this.denom * that.denom)
   }
 
-  override def toString = numer + "/" + denom
+  def <(that: Rational): Boolean = {
+    this.numer * that.denom - that.numer * this.denom < 0
+  }
+
+  def max(that: Rational): Rational = {
+    if (this < that) that else this
+
+  }
+
+  override def toString = {
+
+    def gcd(a: Int, b: Int): Int = {
+      if (b == 0) a else gcd(b, a % b)
+    }
+    val g = gcd(numer, denom)
+    numer / g + "/" + denom / g
+  }
 }
--- AlgorithmsProject/src/in/algorithms/rationals/Rationals.sc
+++ AlgorithmsProject/src/in/algorithms/rationals/Rationals.sc
@@ -3,35 +3,60 @@ package in.algorithms.rationals
 object Rationals {
   val rational1 = new Rational(2, 3)              //> rational1  : in.algorithms.rationals.Rational = 2/3
   val rational2 = new Rational(1, 2)              //> rational2  : in.algorithms.rationals.Rational = 1/2
-  rational1.add(rational2)                        //> res0: in.algorithms.rationals.Rational = 7/6
-  rational1.add(rational2).neg()                  //> res1: in.algorithms.rationals.Rational = -7/6
-  rational1.sub(rational2)                        //> res2: in.algorithms.rationals.Rational = 1/6
+  rational1 + rational2                           //> res0: in.algorithms.rationals.Rational = 7/6
+  rational1 + -rational2                          //> res1: in.algorithms.rationals.Rational = 1/6
+  rational1 - rational2                           //> res2: in.algorithms.rationals.Rational = 1/6
 
   val x = new Rational(1, 3)                      //> x  : in.algorithms.rationals.Rational = 1/3
   val y = new Rational(5, 7)                      //> y  : in.algorithms.rationals.Rational = 5/7
   val z = new Rational(3, 2)                      //> z  : in.algorithms.rationals.Rational = 3/2
-  x.add(y).mul(z)                                 //> res3: in.algorithms.rationals.Rational = 66/42
+
+  x + y * z                                       //> res3: in.algorithms.rationals.Rational = 59/42
+  x < y                                           //> res4: Boolean = true
+  x.max(y)                                        //> res5: in.algorithms.rationals.Rational = 5/7
+
+  val strange = new Rational(1, 1)                //> strange  : in.algorithms.rationals.Rational = 1/1
+  new Rational(5)                                 //> res6: in.algorithms.rationals.Rational = 5/1
 }
 
 class Rational(x: Int, y: Int) {
-  def numer = x
-  def denom = y
+  def this(x: Int) = this(x, 1)
+  require(y != 0, "Denominator must not be zero")
 
-  def add(that: Rational): Rational = {
+  val numer = x
+  val denom = y
+
+  def +(that: Rational): Rational = {
     new Rational(this.numer * that.denom + this.denom * that.numer, this.denom * that.denom)
   }
 
-  def neg(): Rational = {
+  def unary_- : Rational = {
     new Rational(-this.numer, this.denom)
   }
 
-  def sub(that: Rational): Rational = {
-    add(that.neg)
+  def -(that: Rational): Rational = {
+    this + -that
   }
 
-  def mul(that: Rational): Rational = {
+  def *(that: Rational): Rational = {
     new Rational(this.numer * that.numer, this.denom * that.denom)
   }
 
-  override def toString = numer + "/" + denom
+  def <(that: Rational): Boolean = {
+    this.numer * that.denom - that.numer * this.denom < 0
+  }
+
+  def max(that: Rational): Rational = {
+    if (this < that) that else this
+
+  }
+
+  override def toString = {
+
+    def gcd(a: Int, b: Int): Int = {
+      if (b == 0) a else gcd(b, a % b)
+    }
+    val g = gcd(numer, denom)
+    numer / g + "/" + denom / g
+  }
 }
