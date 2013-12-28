7e19d0eb4b96069ffcb0517c99bee8749f51d6d7
Vivek <Vivek@Vivek-PC>
2013-12-28 17:17:34 +0530

    Added insertion sort

---
+++ AlgorithmsProject/src/in/algorithms/insertionsort/InsertionSort.scala
@@ -0,0 +1,21 @@
+package in.algorithms.insertionsort
+
+object InsertionSort {
+  def insertionSort(xs: List[Int]): List[Int] = {
+    xs match {
+      case List() => List()
+      case y :: ys => insert(y, insertionSort(ys))
+    }
+  }
+
+  def insert(x: Int, xs: List[Int]): List[Int] = {
+    xs match {
+      case List() => List(x)
+      case y :: ys => if (x < y) x :: xs else y :: insert(x, ys)
+    }
+  }
+  def main(args: Array[String]) {
+    println(insertionSort(List(2, 1, 3, 6, 5, 4)))
+  }
+
+}
