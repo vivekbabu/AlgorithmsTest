375f6a11a79932d983be3308b28ed6017d66e0e7
Vivek <Vivek@Vivek-PC>
2014-01-19 23:21:51 +0530

    Added Data Structures Stack and its tests

---
+++ AlgorithmsProject/src/in/algorithms/implementeddatastructures/Queue.java
@@ -0,0 +1,12 @@
+package in.algorithms.implementeddatastructures;
+
+public class Queue<T> {
+	public Node<T> front=null, rear=null;
+	
+	public Queue<T> enqueue() {
+		
+		return this;
+	}
+	
+	
+}
