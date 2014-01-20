a4663ab20a9309e8bf57ffe82cbd75f28138509f
vivekbabu <vivek.babu@gmail.com>
2014-01-20 17:22:49 +0530

    Find the nth last node

---
+++ AlgorithmsProject/src/in/algorithms/findnthlastnode/FindNthLastNode.java
@@ -0,0 +1,11 @@
+package in.algorithms.findnthlastnode;
+
+import in.algorithms.implementeddatastructures.Stack;
+
+public class FindNthLastNode {
+	
+	
+	public static void main(String[] args) {
+		Stack<Integer> linkedlist = new Stack<Integer>();
+	}
+}
