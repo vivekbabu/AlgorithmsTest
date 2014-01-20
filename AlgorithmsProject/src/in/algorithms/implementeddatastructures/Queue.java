86b96f1e37602465cb68a28fa291c2e4b8d3602e
vivekbabu <vivek.babu@gmail.com>
2014-01-20 14:35:06 +0530

    Implemented Queue tests

--- AlgorithmsProject/src/in/algorithms/implementeddatastructures/Queue.java
+++ AlgorithmsProject/src/in/algorithms/implementeddatastructures/Queue.java
@@ -1,41 +1,40 @@
 package in.algorithms.implementeddatastructures;
 
 public class Queue<T> {
-	private Node<T> front=null, rear=null;
-	
+	private Node<T> front = null, rear = null;
+
 	public Queue<T> enqueue(T item) {
 		Node<T> newElement = new Node<T>(item);
-		if(front==null) {
+		if (front == null) {
 			front = rear = newElement;
-		}
-		else {
+		} else {
 			rear.next = newElement;
 			rear = newElement;
 		}
 		return this;
 	}
-	
+
 	public T dequeue() {
-		T  frontElement = null;
-		if(front!=null) {
+		T frontElement = null;
+		if (front != null) {
 			frontElement = front.item;
-			if(front == rear) {
+			if (front == rear) {
 				front = rear = null;
-			}
-			else {
+			} else {
 				front = front.next;
 			}
 		}
-		
+
 		return frontElement;
 	}
-	
+
 	public void printElements() {
 		Node<T> forwardPointer = front;
-		while(forwardPointer !=null) {
+		while (forwardPointer != null) {
 			System.out.print(forwardPointer.item + " ");
+			forwardPointer = forwardPointer.next;
 		}
 		System.out.println();
 	}
-	
+
 }
--- AlgorithmsProject/src/in/algorithms/implementeddatastructures/test/QueueTester.java
+++ AlgorithmsProject/src/in/algorithms/implementeddatastructures/test/QueueTester.java
@@ -6,6 +6,19 @@ public class QueueTester {
 	
 	public static void main(String[] args) {
 		Queue<Integer> queue = new Queue<Integer>();
-		queue.enqueue(item)
+		queue.enqueue(1).enqueue(2).enqueue(3).enqueue(4).enqueue(5);
+		queue.printElements();
+		for(int i=0;i<4; i++) {
+			System.out.println("Dequeued " + queue.dequeue());
+		}
+		queue.printElements();
+		queue.enqueue(1).enqueue(2).enqueue(3).enqueue(4);
+		queue.printElements();
+		for(int i=0;i<5; i++) {
+			System.out.println("Dequeued " + queue.dequeue());
+		}
+		queue.printElements();
+		queue.enqueue(1);
+		queue.printElements();
 	}
 }
