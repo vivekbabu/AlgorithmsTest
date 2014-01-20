e6f78c8d99e2a2183df4ca014fbf07cd27989f78
Vivek <Vivek@Vivek-PC>
2014-01-20 09:59:33 +0530

    Implemented Queue

--- AlgorithmsProject/src/in/algorithms/implementeddatastructures/Queue.java
+++ AlgorithmsProject/src/in/algorithms/implementeddatastructures/Queue.java
@@ -1,12 +1,41 @@
 package in.algorithms.implementeddatastructures;
 
 public class Queue<T> {
-	public Node<T> front=null, rear=null;
+	private Node<T> front=null, rear=null;
 	
-	public Queue<T> enqueue() {
-		
+	public Queue<T> enqueue(T item) {
+		Node<T> newElement = new Node<T>(item);
+		if(front==null) {
+			front = rear = newElement;
+		}
+		else {
+			rear.next = newElement;
+			rear = newElement;
+		}
 		return this;
 	}
 	
+	public T dequeue() {
+		T  frontElement = null;
+		if(front!=null) {
+			frontElement = front.item;
+			if(front == rear) {
+				front = rear = null;
+			}
+			else {
+				front = front.next;
+			}
+		}
+		
+		return frontElement;
+	}
+	
+	public void printElements() {
+		Node<T> forwardPointer = front;
+		while(forwardPointer !=null) {
+			System.out.print(forwardPointer.item + " ");
+		}
+		System.out.println();
+	}
 	
 }
---
+++ AlgorithmsProject/src/in/algorithms/implementeddatastructures/test/QueueTester.java
@@ -0,0 +1,11 @@
+package in.algorithms.implementeddatastructures.test;
+
+import in.algorithms.implementeddatastructures.Queue;
+
+public class QueueTester {
+	
+	public static void main(String[] args) {
+		Queue<Integer> queue = new Queue<Integer>();
+		queue.enqueue(item)
+	}
+}
