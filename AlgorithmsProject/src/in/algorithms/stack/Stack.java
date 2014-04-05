386e08e67f42c72fe907444de599ace1b3dafbf2
Vivek <Vivek@Vivek-PC>
2014-04-05 07:06:28 +0530

    Added the stack with min

---
+++ AlgorithmsProject/src/in/algorithms/stack/Stack.java
@@ -0,0 +1,36 @@
+package in.algorithms.stack;
+
+public class Stack<T>{
+	Node<T> head = null;
+	class Node<T> {
+		T value;
+		Node<T> next;
+	}
+	
+	public Stack<T> push(T element) {
+		Node<T> stackElemnt = new Node<T>();
+		stackElemnt.value = element;
+		stackElemnt.next = head;
+		head = stackElemnt;
+		return this;
+	}
+	
+	public T pop() {
+		if(head == null) return null;
+		else {
+			T valueToReturn = head.value;
+			head = head.next;
+			return valueToReturn;
+		}
+	}
+	
+	public void printStack() {
+		Node<T> top = head;
+		while(top !=null) {
+			System.out.print(top.value+"->");
+			top = top.next;
+		}
+		System.out.println("");
+	}
+	
+}
