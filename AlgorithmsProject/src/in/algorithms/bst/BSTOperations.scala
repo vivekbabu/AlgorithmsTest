58fd888c18c8f6b2a3d2649f8e0f7ae14b40198f
vivekbabu <vivek.babu@gmail.com>
2014-04-21 16:13:46 +0530

    Added the spiral model printing

--- AlgorithmsProject/src/in/algorithms/bst/BSTOperations.scala
+++ AlgorithmsProject/src/in/algorithms/bst/BSTOperations.scala
@@ -1,14 +1,14 @@
 package in.algorithms.bst
 
 class BSTOperations {
-  
+
   def inOrder(root: BSTNode): Unit = {
     if (root != null) {
       inOrder(root.lchild)
       System.out.print(root.value + " ")
       inOrder(root.rchild)
     }
-  } 
+  }
 
   def preOrder(root: BSTNode): Unit = {
     if (root != null) {
@@ -17,7 +17,7 @@ class BSTOperations {
       preOrder(root.rchild)
 
     }
-  } 
+  }
 
   def postOrder(root: BSTNode): Unit = {
     if (root != null) {
@@ -27,53 +27,83 @@ class BSTOperations {
 
     }
   }
-  
-  def checkIfBST(root: BSTNode) : Boolean = {
-   var isBST = true
-   var maxCurrent: Int = Integer.MIN_VALUE
-   def BSTChecker(root: BSTNode): Unit = {
-    if (root != null && isBST) {
-      BSTChecker(root.lchild)
-      if (root.value < maxCurrent) {
-        isBST = false
-      } else {
-        maxCurrent = root.value
+
+  def checkIfBST(root: BSTNode): Boolean = {
+    var isBST = true
+    var maxCurrent: Int = Integer.MIN_VALUE
+    def BSTChecker(root: BSTNode): Unit = {
+      if (root != null && isBST) {
+        BSTChecker(root.lchild)
+        if (root.value < maxCurrent) {
+          isBST = false
+        } else {
+          maxCurrent = root.value
+        }
+        BSTChecker(root.rchild)
       }
-      BSTChecker(root.rchild)
     }
-  }
-   BSTChecker(root)
+    BSTChecker(root)
     isBST
   }
-  
-  def convertToBST(root : BSTNode) : BSTNode = {
-    var head : BSTNode = null
-    var prev : BSTNode = null
-    def BSTConvert(root : BSTNode) : Unit = {
-      if(root == null) 
+
+  def convertToBST(root: BSTNode): BSTNode = {
+    var head: BSTNode = null
+    var prev: BSTNode = null
+    def BSTConvert(root: BSTNode): Unit = {
+      if (root == null)
         return
       else {
         BSTConvert(root.lchild)
-        if(prev == null) 
+        if (prev == null)
           head = root
-         else {
-           root.lchild = prev
-           prev.rchild = root
-         }
+        else {
+          root.lchild = prev
+          prev.rchild = root
+        }
         prev = root
         BSTConvert(root.rchild)
-      } 
+      }
     }
     BSTConvert(root)
     head
   }
-  
-  def printInSpiralModel(root : BSTNode) : Unit = {
-    
-//    var leftToRight = 
-    
-    
-    
+
+  def printInSpiralModel(root: BSTNode, initialLeftToRightValue : Boolean): Unit = {
+
+    var leftToRight = initialLeftToRightValue
+    val height = getTreeHeight(root)
+    println("Height is " + height)
+
+    def printSpiral(root: BSTNode, height: Int): Unit = {
+      if (root == null)
+        return
+      else if (height == 1)
+        print(root.value + " ")
+      else {
+        if (leftToRight) {
+          printSpiral(root.lchild, height - 1)
+          printSpiral(root.rchild, height - 1)
+        } else {
+          printSpiral(root.rchild, height - 1)
+          printSpiral(root.lchild, height - 1)
+        }
+      }
+    }
+
+    for (i <- 1 to height) {
+       printSpiral(root, i)
+      leftToRight = !leftToRight
+    }
+     
+  }
+
+  def getTreeHeight(root: BSTNode): Int = {
+    if (root == null)
+      0
+    else {
+      1 + Math.max(getTreeHeight(root.lchild), getTreeHeight(root.rchild))
+    }
+
   }
 
 }
---
+++ AlgorithmsProject/src/in/algorithms/bst/PrintSpiralModel.sc
@@ -0,0 +1,17 @@
+package in.algorithms.bst
+
+object PrintSpiralModel {
+  println("Welcome to the Scala worksheet")       //> Welcome to the Scala worksheet
+  
+    val root1 = new BSTNode(4,
+    new BSTNode(2, new BSTNode(1, null, null), new BSTNode(3, null, null)),
+    new BSTNode(6, new BSTNode(5, null, null), new BSTNode(7, null, null)))
+                                                  //> root1  : in.algorithms.bst.BSTNode = in.algorithms.bst.BSTNode@46fa55e6
+     var bst = new BSTOperations                  //> bst  : in.algorithms.bst.BSTOperations = in.algorithms.bst.BSTOperations@4e4
+                                                  //| 60434
+     bst.printInSpiralModel(root1, true);println  //> Height is 3
+                                                  //| 4 6 2 1 3 5 7 
+      bst.printInSpiralModel(root1, false);println//> Height is 3
+                                                  //| 4 2 6 7 5 3 1 
+    
+}
