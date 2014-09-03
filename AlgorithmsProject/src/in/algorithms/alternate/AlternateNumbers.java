435858243db5c9e2a00db5ac63cd1fee602b3078
Vivek <Vivek@Vivek-PC>
2014-09-03 10:42:32 +0530

    Impltemented the Alternate numbers full code

--- AlgorithmsProject/src/in/algorithms/alternate/AlternateNumbers.java
+++ AlgorithmsProject/src/in/algorithms/alternate/AlternateNumbers.java
@@ -2,8 +2,40 @@ package in.algorithms.alternate;
 
 public class AlternateNumbers {
 
-  public void alternateTheNumbers(Integer [] elements) {
-    
-  }
+	public void alternateTheNumbers(Integer[] elements) {
+		int negative = -1, positive = 0;
+		while (positive < elements.length) {
+			if (elements[positive] < 0) {
+				negative++;
+				swap(elements, negative, positive);
+			}
+			positive = positive + 1;
+		}
+		positive = negative + 1;
+		negative = 0;
+	
+		while(positive < elements.length && negative < positive && elements[negative] < 0) {
+			swap(elements,negative, positive);
+			negative += 2;
+			positive ++;
+		}
+		
+	}
+
+	
+
+	private void swap(Integer[] elements, int current, int forward) {
+		int temp = elements[current];
+		elements[current] = elements[forward];
+		elements[forward] = temp;
+	}
+	
+	public static void main(String[] args) {
+		Integer[] elements = new Integer[] {-1, 2, -3, 4, 5, 6, -7, 8, 9};
+		new AlternateNumbers().alternateTheNumbers(elements);
+		for(int i = 0; i < elements.length ; i++) System.out.print(elements[i] + " ");
+	}
+	
+	
 
 }
