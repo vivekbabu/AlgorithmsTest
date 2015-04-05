53d8e3b10dab725c259f34197bb944755c226f01
vivekbabu <vivek.babu@gmail.com>
2015-04-05 15:08:44 +0530

    Added Word Count

---
+++ AlgorithmsProject/src/in/algorithms/wordcount/WordCount.scala
@@ -0,0 +1,9 @@
+package in.algorithms.wordcount
+
+object WordCount {
+	def main(args: Array[String]) {
+		println(scala.io.Source.fromURL("http://docs.scala-lang.org/overviews/parallel-collections/custom-parallel-collections.html")
+		.getLines.flatMap(_.split(" "))
+		.foldLeft(Map[String,Int]())((count, word) => count + (word ->(count.getOrElse(word, 0)+1))))
+}
+}
