c62a80c48845cbdb51c0da4b77019e1d4e9ec2d1
vivekbabu <vivek.babu@gmail.com>
2015-04-05 16:06:32 +0530

    Added lowercase check to get consistent results for words

--- AlgorithmsProject/src/in/algorithms/wordcount/WordCount.scala
+++ AlgorithmsProject/src/in/algorithms/wordcount/WordCount.scala
@@ -4,6 +4,6 @@ object WordCount {
 	def main(args: Array[String]) {
 		println(scala.io.Source.fromURL("http://docs.scala-lang.org/overviews/parallel-collections/custom-parallel-collections.html")
 		.getLines.flatMap(_.split(" "))
-		.foldLeft(Map[String,Int]())((count, word) => count + (word ->(count.getOrElse(word, 0)+1))))
+		.foldLeft(Map[String,Int]())((count, word) => count + (word.toLowerCase() ->(count.getOrElse(word.toLowerCase(), 0)+1))))
 }
 }
